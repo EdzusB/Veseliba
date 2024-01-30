c66d3b1ef2a5f3e12cd919a283e0432348a0f9ff
IntarsI <ilgsalietisintars@gmail.com>
2024-01-30 09:53:25 +0200

    Pievienoju klases
    Pievienoju klases

--- prikoligais/Form1.cs
+++ prikoligais/Form1.cs
@@ -28,15 +28,10 @@ namespace prikoligais
 
         private void registracijas_podzina_Click(object sender, EventArgs e)
         {
+
         }
 
-        private void PievienotFiziskoAktivitati()
-        {
-            SQLiteConnection sqlite_conn = datubazes_konekcija();
-            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
-            sqlite_cmd.CommandText = "INSERT INTO fiziska_aktivitate (akt_veids, akt_ilgums) VALUES('" + this.akt_veids.Text + "', '" + this.akt_ilgums.Text + "')";
-            sqlite_cmd.ExecuteNonQuery();
-        }
+
 
         private void PievienotLietotaju()
         {
@@ -46,23 +41,9 @@ namespace prikoligais
             sqlite_cmd.ExecuteNonQuery();
         }
 
-        private void PievienotMiegu()
-        {
-            SQLiteConnection sqlite_conn = datubazes_konekcija();
-            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
-            sqlite_cmd.CommandText = "INSERT INTO miegs (miega_ilgums) VALUES('" + this.miega_ilgums.Text + "')";
-            sqlite_cmd.ExecuteNonQuery();
-        }
 
-        private void PievienotUzturu()
-        {
-            SQLiteConnection sqlite_conn = datubazes_konekcija();
-            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
 
-            sqlite_cmd.CommandText = "INSERT INTO uzturs (kalorijas, ediens, izdevumi) VALUES ('" + this.kalorijas.Text + "', '" + this.ediens.Text + "', '" + this.izdevumi.Text + "')";
 
-            sqlite_cmd.ExecuteNonQuery();
-        }
 
         private string Ieteikumi()
         {
@@ -76,10 +57,21 @@ namespace prikoligais
 
         private void registracijas_podzina_Click_1(object sender, EventArgs e)
         {
-            PievienotUzturu();
-            PievienotFiziskoAktivitati();
+            aktivitates akt = new aktivitates();
+            akt.aktivitesilgums = akt_ilgums.Text;
+            akt.aktivitatesveids = akt_veids.Text;
+            akt.PievienotFiziskoAktivitati();
+            akt.ieteikums();
+            uzturs uzt = new uzturs();
+            uzt.uznemtaskal = kalorijas.Text;
+            uzt.uznemtaisediens = ediens.Text;
+            uzt.nauda = izdevumi.Text;
+            uzt.miegailgums = miega_ilgums.Text;
+            uzt.ieteikums();
+            uzt.PievienotMiegu();
+            uzt.PievienotUzturu();
             PievienotLietotaju();
-            PievienotMiegu();
+
 
             MessageBox.Show("Dati veiksmīgi ievadīti!");
         }
@@ -90,5 +82,56 @@ namespace prikoligais
             f2.Show();
             Visible = false;
         }
+
+        private void miega_ilgums_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        class aktivitates
+        {
+            public string aktivitesilgums { get; set; }
+            public string aktivitatesveids { get; set; }
+            public void PievienotFiziskoAktivitati()
+            {
+                SQLiteConnection sqlite_conn = datubazes_konekcija();
+                SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "INSERT INTO fiziska_aktivitate (akt_veids, akt_ilgums) VALUES('" + aktivitesilgums + "', '" + aktivitatesveids + "')";
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            public void ieteikums()
+            {
+                //pēc noteiktiem nosacījumiem izlasa no DB ieteikumu un izvada to uz ekrāna
+            }
+
+        }
+        class uzturs
+        {
+            public string uznemtaskal { get; set; }
+            public string uznemtaisediens { get; set; }
+            public string nauda { get; set; }
+            public string miegailgums { get; set; }
+            public void PievienotMiegu()
+            {
+                SQLiteConnection sqlite_conn = datubazes_konekcija();
+                SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "INSERT INTO miegs (miega_ilgums) VALUES('" + miegailgums + "')";
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            public void PievienotUzturu()
+            {
+                SQLiteConnection sqlite_conn = datubazes_konekcija();
+                SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+
+                sqlite_cmd.CommandText = "INSERT INTO uzturs (kalorijas, ediens, izdevumi) VALUES ('" + uznemtaskal + "', '" + uznemtaisediens + "', '" + nauda + "')";
+
+                sqlite_cmd.ExecuteNonQuery();
+            }
+
+            public void ieteikums()
+            {
+                //pēc noteiktiem nosacījumiem izlasa no DB ieteikumu un izvada to uz ekrāna
+            }
+        }
     }
 }
+
