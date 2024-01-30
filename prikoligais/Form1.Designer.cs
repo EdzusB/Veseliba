87b97a0650d8cfb6e41709095e2c3dd8e7072961
EdzusB <edzusbleikss1@gmail.com>
2024-01-30 08:32:19 +0200

    Pirmā lejupielāde

---
+++ prikoligais/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace prikoligais
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.akt_veids = new System.Windows.Forms.TextBox();
+            this.ediens = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.izdevumi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.registracijas_podzina = new System.Windows.Forms.Button();
+            this.akt_ilgums = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.svars = new System.Windows.Forms.TextBox();
+            this.augums = new System.Windows.Forms.TextBox();
+            this.dzimums = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.miega_ilgums = new System.Windows.Forms.TextBox();
+            this.kalorijas = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.ieteikumi = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // akt_veids
+            // 
+            this.akt_veids.Location = new System.Drawing.Point(191, 246);
+            this.akt_veids.Name = "akt_veids";
+            this.akt_veids.Size = new System.Drawing.Size(163, 20);
+            this.akt_veids.TabIndex = 0;
+            // 
+            // ediens
+            // 
+            this.ediens.Location = new System.Drawing.Point(525, 177);
+            this.ediens.Name = "ediens";
+            this.ediens.Size = new System.Drawing.Size(163, 20);
+            this.ediens.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aktivitātes veids:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(479, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ēdiens:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(186, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ieteikumi:";
+            // 
+            // izdevumi
+            // 
+            this.izdevumi.Location = new System.Drawing.Point(525, 250);
+            this.izdevumi.Name = "izdevumi";
+            this.izdevumi.Size = new System.Drawing.Size(163, 20);
+            this.izdevumi.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(446, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Izdevumi(eiro):";
+            // 
+            // registracijas_podzina
+            // 
+            this.registracijas_podzina.Location = new System.Drawing.Point(578, 320);
+            this.registracijas_podzina.Name = "registracijas_podzina";
+            this.registracijas_podzina.Size = new System.Drawing.Size(179, 76);
+            this.registracijas_podzina.TabIndex = 2;
+            this.registracijas_podzina.Text = "Reģistrēt";
+            this.registracijas_podzina.UseVisualStyleBackColor = true;
+            // 
+            // akt_ilgums
+            // 
+            this.akt_ilgums.Location = new System.Drawing.Point(191, 284);
+            this.akt_ilgums.Name = "akt_ilgums";
+            this.akt_ilgums.Size = new System.Drawing.Size(163, 20);
+            this.akt_ilgums.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Fiziskās aktivitātes ilgums(stundas):";
+            // 
+            // svars
+            // 
+            this.svars.Location = new System.Drawing.Point(191, 67);
+            this.svars.Name = "svars";
+            this.svars.Size = new System.Drawing.Size(163, 20);
+            this.svars.TabIndex = 0;
+            // 
+            // augums
+            // 
+            this.augums.Location = new System.Drawing.Point(191, 105);
+            this.augums.Name = "augums";
+            this.augums.Size = new System.Drawing.Size(163, 20);
+            this.augums.TabIndex = 0;
+            // 
+            // dzimums
+            // 
+            this.dzimums.Location = new System.Drawing.Point(191, 143);
+            this.dzimums.Name = "dzimums";
+            this.dzimums.Size = new System.Drawing.Size(163, 20);
+            this.dzimums.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(133, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Svars(kg):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Augums(cm):";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(133, 146);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Dzimums:";
+            // 
+            // miega_ilgums
+            // 
+            this.miega_ilgums.Location = new System.Drawing.Point(525, 140);
+            this.miega_ilgums.Name = "miega_ilgums";
+            this.miega_ilgums.Size = new System.Drawing.Size(163, 20);
+            this.miega_ilgums.TabIndex = 0;
+            // 
+            // kalorijas
+            // 
+            this.kalorijas.Location = new System.Drawing.Point(525, 210);
+            this.kalorijas.Name = "kalorijas";
+            this.kalorijas.Size = new System.Drawing.Size(163, 20);
+            this.kalorijas.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(407, 143);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(114, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Miega ilgums(stundās):";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(467, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Kalorijas:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label12.Location = new System.Drawing.Point(186, 211);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(187, 29);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Fiziskā aktivitāte";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label13.Location = new System.Drawing.Point(216, 35);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(103, 29);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "Lietotājs";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label14.Location = new System.Drawing.Point(456, 105);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(252, 29);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "Uzturs, miega kvalitāte";
+            // 
+            // ieteikumi
+            // 
+            this.ieteikumi.Location = new System.Drawing.Point(295, 342);
+            this.ieteikumi.Name = "ieteikumi";
+            this.ieteikumi.Size = new System.Drawing.Size(196, 55);
+            this.ieteikumi.TabIndex = 4;
+            this.ieteikumi.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(786, 400);
+            this.Controls.Add(this.ieteikumi);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.registracijas_podzina);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kalorijas);
+            this.Controls.Add(this.izdevumi);
+            this.Controls.Add(this.miega_ilgums);
+            this.Controls.Add(this.dzimums);
+            this.Controls.Add(this.augums);
+            this.Controls.Add(this.ediens);
+            this.Controls.Add(this.svars);
+            this.Controls.Add(this.akt_ilgums);
+            this.Controls.Add(this.akt_veids);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox akt_veids;
+        private System.Windows.Forms.TextBox ediens;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox izdevumi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button registracijas_podzina;
+        private System.Windows.Forms.TextBox akt_ilgums;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox svars;
+        private System.Windows.Forms.TextBox augums;
+        private System.Windows.Forms.TextBox dzimums;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox miega_ilgums;
+        private System.Windows.Forms.TextBox kalorijas;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.RichTextBox ieteikumi;
+    }
+}
+
---
+++ prikoligais/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace prikoligais
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static SQLiteConnection datubazes_konekcija()
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=datubaze.db; Version = 3; New = True; Compress = True;");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+            }
+            return sqlite_conn;
+        }
+
+        private void registracijas_podzina_Click(object sender, EventArgs e)
+        {
+            if (svars.Text != "")
+            {
+                PievienotFiziskoAktivitati();
+                PievienotLietotaju();
+                PievienotMiegu();
+                PievienotUzturu();
+
+                MessageBox.Show("Informācija mēmākā, jaunākā ievadīta!");
+            }
+            else
+            {
+                MessageBox.Show("Lūdzu aizpildiet dotos laukus!");
+            }
+        }
+
+        private void PievienotFiziskoAktivitati()
+        {
+            SQLiteConnection sqlite_conn = datubazes_konekcija();
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO fiziska_aktivitate (akt_veids, akt_ilgums) VALUES('" + this.akt_veids.Text + "', '" + this.akt_ilgums.Text + "') ";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        private void PievienotLietotaju()
+        {
+            SQLiteConnection sqlite_conn = datubazes_konekcija();
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO lietotajs (svars, augums, dzimums) VALUES('" + this.svars.Text + "', '" + this.augums.Text + "', '" + this.dzimums.Text + "') ";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        private void PievienotMiegu()
+        {
+            SQLiteConnection sqlite_conn = datubazes_konekcija();
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO miegs (miega_ilgums) VALUES('" + this.miega_ilgums.Text + "')";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        private void PievienotUzturu()
+        {
+            SQLiteConnection sqlite_conn = datubazes_konekcija();
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO uzturs (kalorijas, ediens, izdevumi) VALUES('" + this.kalorijas.Text + this.ediens.Text + this.izdevumi.Text + "') ";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        private string Ieteikumi()
+        {
+            return "Ieteikumi: Veiciet vairāk fiziskās aktivitātes un uzmanieties par veselīgu uzturu!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
